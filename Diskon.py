2186e509b6c9b34b2f1c6da0efc32ae6781d37c5
javiakbar <139321188+javiakbar@users.noreply.github.com>
2023-07-18 22:10:21 +0700

    second commit

--- Diskon.py
+++ Diskon.py
@@ -7,3 +7,9 @@ harga_belanja = int(input("Total Belanja: Rp"))
 if (harga_belanja >= 100000) and (harga_belanja < 500000):
     harga_total = harga_belanja - (0.1 * harga_belanja)
     print("Selamat Anda mendapat diskon 10%")
+if (harga_belanja >= 100000) and (harga_belanja < 500000):
+    harga_total = harga_belanja - (0.1 * harga_belanja)
+    print("Selamat Anda mendapat diskon 10%")
+elif (harga_belanja >= 500000) and (harga_belanja < 1000000):
+    harga_total = harga_belanja - (0.2 * harga_belanja)
+    print("Selamat Anda mendapat diskon 20%")
